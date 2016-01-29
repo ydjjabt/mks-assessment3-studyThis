51b806cdfc47138f71d4c0879f5910360012e52c
Darko <darkogjorgoski@gmail.com>
2016-01-28 16:29:01 -0800

    added push jon

--- backbone-pond-view/pondView.js
+++ backbone-pond-view/pondView.js
@@ -2,4 +2,27 @@ var PondView = Backbone.View.extend({
 	/* Fill out your solution here */
   tagName: 'table',
 
+  initialize:function() {
+    this.render();
+    this.collection.on("change:name", function() {
+      this.render();
+    }.bind(this));
+  },
+
+
+
+  _template: _.template("<tr><td><%=name%></td><td>" + "<image src='<%=image%>'></td></tr"),
+
+
+  render:function() {
+    this.$el.empty();
+    var self = this;
+    this.collection.map(function(fish) {
+
+      // var fishHtml = "<div>fdsfsd"+ fish.get("name") + "</div";
+      var fishHtml = self._template(fish.attributes);
+      self.$el.append(fishHtml);
+    });
+  }
+
 });
